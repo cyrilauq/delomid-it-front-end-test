3226ee543345554cfcd07f16b32a2eb911c13ce5
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-12-26 18:40:55 +0100

    add contact part

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
 var navElement = document.getElementsByClassName("navbar")[0];
+var newsLettersEmailForm = document.getElementById("newsletter-form");
+var emailNewsLetterField = document.querySelector("input[name=\"email\"]");
+var emailError = document.getElementById("email_error");
+
+const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
 
 window.addEventListener('scroll', scrollListener);
 window.addEventListener('load', scrollListener);
+
+newsLettersEmailForm.addEventListener("submit", (event) => {
+    subscribeToNewsLetter(event);
+})
 
 function scrollListener() {
     if(hasScrolled()) {
@@ -12,5 +21,17 @@ function scrollListener() {
 }
 
 function hasScrolled() {
-    return document.scrollingElement.scrollTop > 0;
+    console.log(document.scrollingElement.scrollTop)
+    return document.scrollingElement.scrollTop !== 0;
+}
+
+function subscribeToNewsLetter(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    
+    if(!emailRegex.test(emailNewsLetterField.value)) {
+        emailError.style.display = "block";
+    } else {
+        emailError.style.display = "none";
+    }
 }
